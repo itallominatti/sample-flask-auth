dbedee91a16975047ef183e66864d90de485b85e
itallominatti <itallominatti@gmail.com>
2024-09-22 17:07:40 -0300

    feat main -> Implementação de perfis, criando regra de negócio para rota update e delete

--- app.py
+++ app.py
@@ -60,6 +60,10 @@ def update_user(id_user):
     data = request.json
     user = User.query.get(id_user)
 
+    if id_user != current_user.id and current_user.role != 'admin':
+        return jsonify({"message": "Você não possui permissão para editar outro usuário"}), 403
+
+
     if user and data.get('password'):
         user.password = data.get('password')
         db.session.commit()
@@ -73,6 +77,9 @@ def update_user(id_user):
 @login_required
 def delete_user(id_user):
     user = User.query.get(id_user)
+
+    if current_user.role != 'admin':
+        return jsonify({"message": "Você não possui permissão para deletar usuários"}), 403
 
     if id_user == current_user.id:
         return jsonify({"message": "não é possível deletar o próprio usuário"}), 403
@@ -91,7 +98,7 @@ def create_user():
     password = data.get('password')
 
     if username and password:
-        user =  User(username, password)
+        user =  User(username=username, password=password, role='user')
         db.session.add(user)
         db.session.commit()
         return jsonify({"message": "usuario criado com sucesso"}), 201
