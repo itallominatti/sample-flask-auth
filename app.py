8553a91d815a24111ae0f39e5102e0047b0175c6
itallominatti <itallominatti@gmail.com>
2024-09-22 17:15:11 -0300

    feat main -> criando lógica de criptografia nos passwords

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from models.user import User
 from database import db
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+import bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
@@ -28,7 +29,7 @@ def login():
 
         user = User.query.filter_by(username=username).first()
 
-        if user and user.password == password:
+        if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
             login_user(user)
             return jsonify({"message": "Credenciais válidas"}), 200
 
@@ -98,7 +99,9 @@ def create_user():
     password = data.get('password')
 
     if username and password:
-        user =  User(username=username, password=password, role='user')
+        hashed_password = bcrypt.hashpw(password=str.encode(password),
+            salt=bcrypt.gensalt())
+        user =  User(username=username, password=hashed_password, role='user')
         db.session.add(user)
         db.session.commit()
         return jsonify({"message": "usuario criado com sucesso"}), 201
