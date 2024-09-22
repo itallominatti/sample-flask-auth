f689f08aff392c848e3ba279e4734c1b6dee6c87
itallominatti <itallominatti@gmail.com>
2024-09-22 16:31:36 -0300

    feat main -> criando rota GET_USER, PUT_USER e READ_USER

--- app.py
+++ app.py
@@ -43,6 +43,31 @@ def logout():
 
 
 
+@app.route('/user/<int:id_user>', methods=['GET'])
+@login_required
+def read_user(id_user):
+    user = User.query.get(id_user)
+
+    if user:
+        return {"username": user.username}, 200
+    
+    return jsonify({"message": "usuario não encontrado"}), 404
+
+
+@app.route('/user/<int:id_user>', methods=['PUT'])
+@login_required
+def update_user(id_user):
+    data = request.json
+    user = User.query.get(id_user)
+
+    if user and data.get('password'):
+        user.password = data.get('password')
+        db.session.commit()
+
+        return jsonify({"message": f"usuario {id_user} atualizado com sucesso"}), 200
+    
+    return jsonify({"message": "usuario não encontrado"}), 404
+
 @app.route('/user', methods=['POST'])
 def create_user():
     data = request.json
